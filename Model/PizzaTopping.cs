56248d2657dd398d7e47e325a74526a8864296cf
Isaias Castillo <isaias.castillo@utp.ac.pa>
2026-06-25 18:10:02 -0500

    Refactor PizzaTopping model and add new components for Pizza and PizzaTopping; update PizzasToppings page

--- Model/PizzaTopping.cs
+++ Model/PizzaTopping.cs
@@ -1,11 +1,8 @@
 ﻿namespace BlazingPizza
 {
-    public class PizzaTopping
-    {
-        public Topping Topping { get; set; }
-
-        public int ToppingId { get; set; }
-        
-        public int PizzaId { get; set; }
-    }
+   public class PizzaTopping
+{
+    public string Name { get; set; }
+    public string Ingredients { get; set; }
+}
 }
