4ca4c614758d77efa2c75e86d6dac0aee026b82f
Isaias Castillo <isaias.castillo@utp.ac.pa>
2026-06-25 15:01:43 -0500

    Add comments to SpecialsController and PizzaStoreContext for clarity; update GetSpecials method formatting

--- Controllers/ SpecialsController.cs
+++ Controllers/ SpecialsController.cs
@@ -10,6 +10,7 @@ public class SpecialsController : Controller
 {
     private readonly PizzaStoreContext _db;
 
+// El contexto es el objeto que te permite hacer operaciones sobre la base de datos sin escribir SQL manualmente.
     public SpecialsController(PizzaStoreContext db)
     {
         _db = db;
@@ -18,6 +19,8 @@ public class SpecialsController : Controller
     [HttpGet]
     public async Task<ActionResult<List<PizzaSpecial>>> GetSpecials()
     {
-        return (await _db.Specials.ToListAsync()).OrderByDescending(s => s.BasePrice).ToList();
+        return (await _db.Specials.ToListAsync())
+        .OrderByDescending(s => s.BasePrice)
+        .ToList();
     }
 }
--- Datos/PizzaStoreContext.cs
+++ Datos/PizzaStoreContext.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BlazingPizza.Data;
 
+// se conecta a la base de datos y me devuelve la tabla specials.
+// El contexto es el objeto que te permite hacer operaciones sobre la base de datos sin escribir SQL manualmente.
 public class PizzaStoreContext : DbContext
 {
     public PizzaStoreContext(DbContextOptions options) : base(options)
