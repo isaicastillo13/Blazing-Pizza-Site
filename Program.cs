662ff81eb27dfc902887b09e7e133c6809b3c252
Isaias Castillo <isaias.castillo@utp.ac.pa>
2026-06-26 13:56:55 -0500

    Add OrderState service to manage pizza configuration state

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using BlazingPizza.Data;
+using BlazingPizza.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
@@ -8,6 +9,7 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<PizzaService>();// Register the pizzas service
 builder.Services.AddHttpClient();
 builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
+builder.Services.AddScoped<OrderState>();
 
 
 var app = builder.Build();
